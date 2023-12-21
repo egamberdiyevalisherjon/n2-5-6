f56813076abd067440a215e56a442a9a9e49c22d
Alisherjon <egamberdiyevalisherjon@gmail.com>
2023-12-21 10:46:59 +0500

    initial commit

---
+++ src/Components/Product.js
@@ -0,0 +1,15 @@
+function Product() {
+  return (
+    <div className="card">
+      <img
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbGTtQ3FXZb05wCBDUiR2tcW3YgMLU3NPVIjfCRjUExw&s"
+        alt=""
+        className="card-image"
+      />
+      <h2>Tears Perfume</h2>
+      <p>$50.99</p>
+    </div>
+  );
+}
+
+export default Product;
